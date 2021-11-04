f83beeb0c4b6a84392427b1f4be8e86e86e0fc58
Phillip Hooper <Psolen@live.unc.edu>
2021-11-03 20:46:22 -0400

    11/3 Updates

--- exercises/ex06/dictionaries.py
+++ exercises/ex06/dictionaries.py
@@ -1,5 +1,34 @@
 """Practice with dictionaries."""
 
-__author__ = "123456789"
+__author__ = "730247598"
 
 # Define your functions below
+
+
+def invert(invert_list: dict[str, str]) -> dict[str, str]:
+    inverted_list: dict[str, str] = dict()
+    value_list: list = list()
+    
+
+    for k in invert_list:
+        value_list.append(invert_list[k])
+        
+        i: int = 0
+
+        # Output
+        while i < len(value_list):
+            j: int = i + 1
+            while j < len(value_list):
+                if value_list[i] == value_list[j]:
+                    raise KeyError("Duplicate values, cannot duplicate dictionary.")
+                j += 1
+            i += 1
+
+
+        inverted_list[invert_list[k]] = k
+
+    return inverted_list
+
+
+
+
--- exercises/ex06/dictionaries_test.py
+++ exercises/ex06/dictionaries_test.py
@@ -3,4 +3,22 @@
 # TODO: Uncomment the below line when ready to write unit tests
 # from exercises.ex06.dictionaries import invert, favorite_color, count
 
-__author__ = "123456789"
+__author__ = "730247598"
+
+
+from exercises.ex06.dictionaries import invert
+
+
+def test_empty() -> None:
+    """Empty dictionary test."""
+    assert invert({}) == {}
+
+
+def test_basic() -> None:
+    """Standard values test."""
+    assert invert({"UNC" : "Duke"}) == {"Duke" : "UNC"}
+
+
+def test_edge_duplicate_values() -> None:
+    """Standard values test."""
+    assert invert({"Phillip" : "Hooper", "Chad" : "Hooper"}) == KeyError("Duplicate values, cannot duplicate dictionary.")
