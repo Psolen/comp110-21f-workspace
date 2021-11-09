d159c2f47e0b711ae43414e267918c7416d58a5d
Phillip <psolen@live.unc.edu>
2021-11-08 19:11:06 -0500

    Up to date

--- lessons/imports.py
+++ lessons/imports.py
@@ -16,6 +16,7 @@ from lessons.helpers import powerful, THE_ANSWER
 def main() -> None:
     """Entrypoint of program."""
     print(helpers.powerful(2, 4))
+    print(hp.powerful(1, 3))
     print(f"The answer: {helpers.THE_ANSWER}")
     print(powerful(2, 4))
     print(THE_ANSWER)
