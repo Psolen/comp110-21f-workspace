0d6ef91f138f00ff4a3773290563001c11f9cf39
Phillip <psolen@live.unc.edu>
2021-11-11 00:43:38 -0500

    give up 1

--- exercises/ex06/dictionaries.py
+++ exercises/ex06/dictionaries.py
@@ -6,15 +6,8 @@ __author__ = "730247598"
 
 
 def invert(invert_list: dict[str, str]) -> dict[str, str]:
+    """Switches the keys and values in a dictionary."""
     inverted_list: dict[str, str] = dict()
-    value_list: list = list()
-
-
-    for k in invert_list:
-        if invert_list[k] == k:
-
-            raise KeyError("Duplicate keys, cannot invert.")
-
 
     for k in invert_list:
 
@@ -23,5 +16,11 @@ def invert(invert_list: dict[str, str]) -> dict[str, str]:
     return inverted_list
 
 
+def favorite_colors(colors: dict[str, str]) -> str:
+    """Finds the mode value in a dictionary."""
+    fav_color: str = " "
 
+    for color_keys in colors:
+        # 
 
+    return fav_color
